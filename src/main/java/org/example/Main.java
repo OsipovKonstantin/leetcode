4454fac288a7695e4da3cd74297a43d4748e47ce
Константин <zed726804@yandex.ru>
2023-12-16 01:15:51 +0300

    feat: решение задачи 5-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.example.SolutionDay1.twoSum;
 import static org.example.SolutionDay2.findSpecialInteger;
 import static org.example.SolutionDay3.maxProduct;
 import static org.example.SolutionDay4.numSpecial;
+import static org.example.SolutionDay5.destCity;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +15,7 @@ public class Main {
         System.out.println(findSpecialInteger(new int[]{1, 2, 2, 6, 6, 6, 6, 7, 10}));
         System.out.println(maxProduct(new int[]{1, 5, 4, 5}));
         System.out.println(numSpecial(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}));
+        System.out.println(destCity(List.of(List.of("London", "New York"), List.of("New York", "Lima"), List.of("Lima", "Sao Paulo"))));
 
     }
 }
---
+++ src/main/java/org/example/SolutionDay5.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//    https://leetcode.com/problems/destination-city
+//    You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+//    Return the destination city, that is, the city without any path outgoing to another city.
+//    It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+//    Example 1:
+//    Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+//    Output: "Sao Paulo"
+//    Explanation: Starting at "London" city you will reach "Sao Paulo" city which is the destination city.
+//    Your trip consist of: "London" -> "New York" -> "Lima" -> "Sao Paulo".
+//    Example 3:
+//    Input: paths = [["A","Z"]]
+//    Output: "Z"
+public class SolutionDay5 {
+    public static String destCity(List<List<String>> paths) {
+        Set<String> pathCount = new HashSet<>();
+        for (List<String> path : paths) {
+            pathCount.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            if (!pathCount.contains(path.get(1)))
+                return path.get(1);
+        }
+        return "";
+    }
+}
