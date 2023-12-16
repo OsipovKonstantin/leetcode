84076288664d0f9a151a00c4c3ea72807a3521d2
Константин <zed726804@yandex.ru>
2023-12-16 15:00:57 +0300

    feat: решение задачи 6-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import static org.example.SolutionDay2.findSpecialInteger;
 import static org.example.SolutionDay3.maxProduct;
 import static org.example.SolutionDay4.numSpecial;
 import static org.example.SolutionDay5.destCity;
+import static org.example.SolutionDay6.isAnagram;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +17,6 @@ public class Main {
         System.out.println(maxProduct(new int[]{1, 5, 4, 5}));
         System.out.println(numSpecial(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}));
         System.out.println(destCity(List.of(List.of("London", "New York"), List.of("New York", "Lima"), List.of("Lima", "Sao Paulo"))));
-
+        System.out.println(isAnagram("anagram", "nagaram") + "; " + isAnagram("rat", "car"));
     }
 }
