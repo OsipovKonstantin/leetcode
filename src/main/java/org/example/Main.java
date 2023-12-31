0d62a65bec4f3e57d3636a8be11b72014694db5f
Константин <zed726804@yandex.ru>
2023-12-31 18:30:25 +0300

    feat: решение 10-ой задачи 20-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,6 +25,7 @@ import static org.example.SolutionDay20Seventh.numberOfMatches;
 import static org.example.SolutionDay20Sixth.totalMoney;
 import static org.example.SolutionDay20Tenth.countCharacters;
 import static org.example.SolutionDay20Third.inorderTraversal;
+import static org.example.SolutionDay21.maxLengthBetweenEqualCharacters;
 import static org.example.SolutionDay3.maxProduct;
 import static org.example.SolutionDay4.numSpecial;
 import static org.example.SolutionDay5.destCity;
@@ -76,5 +77,6 @@ public class Main {
         System.out.println(largestGoodInteger("6777133339"));
         System.out.println(minTimeToVisitAllPoints(new int[][]{{1, 1}, {3, 4}, {-1, 0}}));
         System.out.println(countCharacters(new String[]{"hello", "world", "leetcode"}, "welldonehoneyr"));
+        System.out.println(maxLengthBetweenEqualCharacters("abca"));
     }
 }
---
+++ src/main/java/org/example/SolutionDay21.java
@@ -0,0 +1,35 @@
+package org.example;
+
+//    easy. String
+//    https://leetcode.com/problems/largest-substring-between-two-equal-characters
+//    Given a string s, return the length of the longest substring between two equal characters, excluding the two
+//    characters. If there is no such substring return -1.
+//    A substring is a contiguous sequence of characters within a string.
+//    Example 1:
+//    Input: s = "aa"
+//    Output: 0
+//    Explanation: The optimal substring here is an empty substring between the two 'a's.
+//    Example 2:
+//    Input: s = "abca"
+//    Output: 2
+//    Explanation: The optimal substring here is "bc".
+//    Example 3:
+//    Input: s = "cbzxy"
+//    Output: -1
+//    Explanation: There are no characters that appear twice in s.
+public class SolutionDay21 {
+    public static int maxLengthBetweenEqualCharacters(String s) {
+        int n = s.length();
+        int[] charCount = new int[26];
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (charCount[c - 'a'] == 0) {
+                charCount[c - 'a'] = i + 1;
+            } else {
+                res = Math.max(res, i - charCount[c - 'a']);
+            }
+        }
+        return res;
+    }
+}
