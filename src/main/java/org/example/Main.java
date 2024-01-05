9d179857a707a1dbf73bfe3e8c4ab10ee270d193
Константин <zed726804@yandex.ru>
2024-01-06 01:35:54 +0300

    feat: решение 3-ей задачи 26-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -49,6 +49,7 @@ import static org.example.SolutionDay25Sixth.countPalindromicSubsequence;
 import static org.example.SolutionDay25Third.minPairSum;
 import static org.example.SolutionDay26.sortVowels;
 import static org.example.SolutionDay26Second.lengthOfLIS;
+import static org.example.SolutionDay26Third.numBusesToDestination;
 import static org.example.SolutionDay3.maxProduct;
 import static org.example.SolutionDay4.numSpecial;
 import static org.example.SolutionDay5.destCity;
@@ -128,6 +129,7 @@ public class Main {
         System.out.println(countPalindromicSubsequence("bbcbaba"));
         System.out.println(sortVowels("lEetcOde"));
         System.out.println(lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
+        System.out.println(numBusesToDestination(new int[][]{{1, 2, 7}, {3, 6, 7}}, 1, 6));
 
         long end = System.currentTimeMillis();
         System.out.println(String.format("длительность всех задач в миллисекундах %d", end - start));
---
+++ src/main/java/org/example/SolutionDay26Third.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.*;
+
+//    hard. Breadth-First Search. First approach (slower than second 10 times, but it is easy to apply).
+//    Steps 1. Map busStop - bus, 2. use queue for iterating buses from source stop to target
+//    https://leetcode.com/problems/bus-routes
+//    You are given an array routes representing bus routes where routes[i] is a bus route that the ith bus repeats
+//    forever.
+//    For example, if routes[0] = [1, 5, 7], this means that the 0th bus travels in the sequence 1 -> 5 -> 7 -> 1 ->
+//    5 -> 7 -> 1 -> ... forever.
+//    You will start at the bus stop source (You are not on any bus initially), and you want to go to the bus stop
+//    target. You can travel between bus stops by buses only.
+//    Return the least number of buses you must take to travel from source to target. Return -1 if it is not possible.
+//    Example 1:
+//    Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
+//    Output: 2
+//    Explanation: The best strategy is take the first bus to the bus stop 7, then take the second bus to the bus stop 6.
+//    Example 2:
+//    Input: routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]], source = 15, target = 12
+//    Output: -1
+public class SolutionDay26Third {
+    public static int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) {
+            return 0;
+        }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < routes.length; i++) {
+            for (int stop : routes[i]) {
+                List<Integer> buses = map.getOrDefault(stop, new ArrayList<>());
+                buses.add(i);
+                map.put(stop, buses);
+            }
+        }
+        if (!map.containsKey(source) || !map.containsKey(target)) {
+            return -1;
+        }
+        Queue<Integer> curr = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        int ans = 1;
+        for (int bus : map.get(source)) {
+            curr.add(bus);
+            visited.add(bus);
+        }
+        while (!curr.isEmpty()) {
+            int size = curr.size();
+            for (int i = 0; i < size; i++) {
+                int currBus = curr.remove();
+                for (int stop : routes[currBus]) {
+                    if (stop == target) {
+                        return ans;
+                    }
+                    for (int nextBus : map.get(stop)) {
+                        if (!visited.contains(nextBus)) {
+                            visited.add(nextBus);
+                            curr.add(nextBus);
+                        }
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+}
