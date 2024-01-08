bda3af9251c4dd61580e59d55bc182a6faa2e597
Константин <zed726804@yandex.ru>
2024-01-09 01:50:54 +0300

    feat: решение 8-ой задачи 29-го дня

--- src/main/java/org/example/SolutionDay29Eighth.java
+++ src/main/java/org/example/SolutionDay29Eighth.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.Arrays;
 
 //    easy. Array custom comparator; Integer.bitCount; stream for primitives and their wrap (int[] and Integer[])
+//    https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits
 //    You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in
 //    their binary representation and in case of two or more integers have the same number of 1's you have to sort
 //    them in ascending order.
