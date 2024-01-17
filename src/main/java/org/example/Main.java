aa2beec5a05073cf1b8eb4bd0dec4838c489c40e
Константин <zed726804@yandex.ru>
2024-01-17 23:01:44 +0300

    feat: решение 4-ой задачи 38-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -94,6 +94,7 @@ import static org.example.SolutionDay37.findInMountainArray;
 import static org.example.SolutionDay37Fifth.searchRange;
 import static org.example.SolutionDay37Third.fullBloomFlowers;
 import static org.example.SolutionDay38.maxDotProduct;
+import static org.example.SolutionDay38Fourth.integerBreak;
 import static org.example.SolutionDay38Second.uniqueOccurrences;
 import static org.example.SolutionDay38Third.numOfArrays;
 import static org.example.SolutionDay4.numSpecial;
@@ -279,6 +280,7 @@ public class Main {
         System.out.println(maxDotProduct(new int[]{2, 1, -2, 5}, new int[]{3, 0, -6}));
         System.out.println(uniqueOccurrences(new int[]{1, 2, 2, 1, 1, 3}));
         System.out.println(numOfArrays(2, 3, 1));
+        System.out.println(integerBreak(10));
 
         long end = System.currentTimeMillis();
         System.out.println(String.format("длительность всех задач в миллисекундах %d", end - start));
