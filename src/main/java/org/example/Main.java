e57f01f7a44da0272aa9b98bda90c9b2386de9f9
Константин <zed726804@yandex.ru>
2024-02-01 18:53:59 +0300

    feat: решение 2-ой задачи 53-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -157,6 +157,7 @@ import static org.example.SolutionDay52Second.dailyTemperatures;
 import static org.example.SolutionDay52Sixth.canCross;
 import static org.example.SolutionDay52Third.minimumReplacement;
 import static org.example.SolutionDay53.divideArray;
+import static org.example.SolutionDay53Second.findLongestChain;
 import static org.example.SolutionDay6.isAnagram;
 import static org.example.SolutionDay8.maxProductDifference;
 import static org.example.SolutionDay9.imageSmoother;
@@ -463,6 +464,7 @@ public class Main {
 
         System.out.println(canCross(new int[]{0, 1, 3, 5, 6, 8, 12, 17}));
         System.out.println(Arrays.deepToString(divideArray(new int[]{1, 3, 4, 8, 7, 9, 3, 5, 1}, 2)));
+        System.out.println(findLongestChain(new int[][]{{1, 2}, {2, 3}, {3, 4}}));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
