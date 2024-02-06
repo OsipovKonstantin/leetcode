ac2eb42efdb81a10fadd301caa4dcb70bcf65b14
Константин <zed726804@yandex.ru>
2024-02-06 23:08:45 +0300

    feat: решение 4-ой задачи 58-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -514,6 +514,7 @@ public class Main {
         System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
         System.out.println(search(new int[]{2, 5, 6, 0, 0, 1, 2}, 0));
         System.out.println(minimizeMax(new int[]{10, 1, 2, 7, 1, 3}, 2));
+        System.out.println(SolutionDay58Fourth.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
