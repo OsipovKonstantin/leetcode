50a95afe0dc6f5c782b26b9261d2f019fd196e87
Константин <zed726804@yandex.ru>
2024-02-06 21:14:59 +0300

    feat: решение 3-ей задачи 58-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -179,6 +179,7 @@ import static org.example.SolutionDay57Sixth.uniquePathsWithObstacles;
 import static org.example.SolutionDay57Third.partition;
 import static org.example.SolutionDay58.groupAnagrams;
 import static org.example.SolutionDay58Second.search;
+import static org.example.SolutionDay58Third.minimizeMax;
 import static org.example.SolutionDay6.isAnagram;
 import static org.example.SolutionDay8.maxProductDifference;
 import static org.example.SolutionDay9.imageSmoother;
@@ -512,6 +513,7 @@ public class Main {
         System.out.println(change(5, new int[]{1, 2, 5}));
         System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
         System.out.println(search(new int[]{2, 5, 6, 0, 0, 1, 2}, 0));
+        System.out.println(minimizeMax(new int[]{10, 1, 2, 7, 1, 3}, 2));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
