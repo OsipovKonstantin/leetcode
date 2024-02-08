d11733a333d49c2ccce907c74ffa3e12bfc11226
Константин <zed726804@yandex.ru>
2024-02-08 23:43:00 +0300

    feat: решение 3-ей задачи 60-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -184,6 +184,7 @@ import static org.example.SolutionDay59Third.frequencySort;
 import static org.example.SolutionDay6.isAnagram;
 import static org.example.SolutionDay60.wordBreak;
 import static org.example.SolutionDay60Second.letterCombinations;
+import static org.example.SolutionDay60Third.numSquares;
 import static org.example.SolutionDay8.maxProductDifference;
 import static org.example.SolutionDay9.imageSmoother;
 
@@ -523,6 +524,7 @@ public class Main {
         System.out.println(generateTrees(3).stream().map(TreeNode::asList).collect(Collectors.toList()));
         System.out.println(wordBreak("leetcode", List.of("leet", "code")));
         System.out.println(letterCombinations("23"));
+        System.out.println(numSquares(12));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
