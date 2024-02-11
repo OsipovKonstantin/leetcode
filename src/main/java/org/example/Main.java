11cfce16aa23e887ccab8bef96a882f0b3f2fec2
Константин <zed726804@yandex.ru>
2024-02-11 12:22:27 +0300

    feat: решение 1-ой задачи 63-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -191,6 +191,7 @@ import static org.example.SolutionDay62Fifth.strangePrinter;
 import static org.example.SolutionDay62Fourth.minimumDeleteSum;
 import static org.example.SolutionDay62Second.permute;
 import static org.example.SolutionDay62Third.combine;
+import static org.example.SolutionDay63.cherryPickup;
 import static org.example.SolutionDay8.maxProductDifference;
 import static org.example.SolutionDay9.imageSmoother;
 
@@ -537,6 +538,7 @@ public class Main {
         System.out.println(combine(4, 2));
         System.out.println(minimumDeleteSum("sea", "eat"));
         System.out.println(strangePrinter("aaabbb"));
+        System.out.println(cherryPickup(new int[][]{{3, 1, 1}, {2, 5, 1}, {1, 5, 5}, {2, 1, 1}}));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
