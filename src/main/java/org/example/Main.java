c21844638576a36a99802e0fdba74dfdd8539521
Константин <zed726804@yandex.ru>
2024-02-17 14:55:01 +0300

    feat: решение 2-ой задачи 69-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -208,6 +208,7 @@ import static org.example.SolutionDay67Third.largestPerimeter;
 import static org.example.SolutionDay68.eraseOverlapIntervals;
 import static org.example.SolutionDay68Second.findLeastNumOfUniqueInts;
 import static org.example.SolutionDay69.addTwoNumbers;
+import static org.example.SolutionDay69Second.furthestBuilding;
 import static org.example.SolutionDay8.maxProductDifference;
 import static org.example.SolutionDay9.imageSmoother;
 
@@ -585,6 +586,7 @@ public class Main {
         System.out.println(String.format("%s %s %s %s %s", lru1, lru2, lru3, lru4, lru5));
         System.out.println(addTwoNumbers(new ListNode(2, new ListNode(4, new ListNode(3))),
                 new ListNode(5, new ListNode(6, new ListNode(4)))).asList());
+        System.out.println(furthestBuilding(new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
