a46626ddf60481c9fb10168b928e06d5ccb231ad
Константин <zed726804@yandex.ru>
2024-02-19 12:45:25 +0300

    feat: решение 2-ой задачи 71-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -214,6 +214,7 @@ import static org.example.SolutionDay70.maxValue;
 import static org.example.SolutionDay70Second.mostBooked;
 import static org.example.SolutionDay70Third.longestSubsequence;
 import static org.example.SolutionDay71.canFinish;
+import static org.example.SolutionDay71Second.isPowerOfTwo;
 import static org.example.SolutionDay8.maxProductDifference;
 import static org.example.SolutionDay9.imageSmoother;
 
@@ -598,6 +599,8 @@ public class Main {
         System.out.println(mostBooked(2, new int[][]{{0, 10}, {1, 5}, {2, 7}, {3, 4}}));
         System.out.println(longestSubsequence(new int[]{1, 2, 3, 4}, 1));
         System.out.println(canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+        System.out.println(isPowerOfTwo(16));
+
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
