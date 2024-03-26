2f66978f41f93f94886f4a711ab98b95abf1788c
Константин <zed726804@yandex.ru>
2024-03-26 16:45:07 +0300

    feat: решение 1-ой задачи 103-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ import static org.example.SolutionDay10.buyChoco;
 import static org.example.SolutionDay100.reverseList;
 import static org.example.SolutionDay101.isPalindrome;
 import static org.example.SolutionDay102.reorderList;
+import static org.example.SolutionDay103.findDuplicates;
 import static org.example.SolutionDay11.maxWidthOfVerticalArea;
 import static org.example.SolutionDay12.maxScore;
 import static org.example.SolutionDay13.isPathCrossing;
@@ -728,6 +729,7 @@ public class Main {
         ListNode listNode102 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         reorderList(listNode102);
         System.out.println(listNode102.asList());
+        System.out.println(findDuplicates(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
