94dae7cffc576c1fe0486c1e64e10b0376322899
Константин <zed726804@yandex.ru>
2024-05-20 23:30:44 +0300

    feat: решение 1-ой задачи 130-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -44,6 +44,7 @@ import static org.example.SolutionDay127.evaluateTree;
 import static org.example.SolutionDay128.removeLeafNodes;
 import static org.example.SolutionDay129.maximumValueSum;
 import static org.example.SolutionDay13.isPathCrossing;
+import static org.example.SolutionDay130.subsetXORSum;
 import static org.example.SolutionDay14.minOperations;
 import static org.example.SolutionDay15.numDecodings;
 import static org.example.SolutionDay16.numRollsToTarget;
@@ -782,6 +783,7 @@ public class Main {
         System.out.println(removeLeafNodes(new TreeNode(1, new TreeNode(2, new TreeNode(2,
                 new TreeNode(2, null, null), null), null), null), 2).asList());
         System.out.println(maximumValueSum(new int[]{2, 3}, 7, new int[][]{{0, 1}}));
+        System.out.println(subsetXORSum(new int[]{5, 1, 6}));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
