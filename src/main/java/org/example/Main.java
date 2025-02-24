fff20282b0a4aecf65aa9fb5db5d188176254f90
Константин <zed726804@yandex.ru>
2025-02-24 22:40:12 +0300

    feat: решение 2-ой задачи 173-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -92,6 +92,7 @@ import static org.example.SolutionDay171Second.recoverFromPreorder;
 import static org.example.SolutionDay172.jump;
 import static org.example.SolutionDay172Second.constructFromPrePost;
 import static org.example.SolutionDay173.hIndex;
+import static org.example.SolutionDay173Second.mostProfitablePath;
 import static org.example.SolutionDay18.getLengthOfOptimalCompression;
 import static org.example.SolutionDay19.minDifficulty;
 import static org.example.SolutionDay2.findSpecialInteger;
@@ -921,6 +922,7 @@ public class Main {
         System.out.println(constructFromPrePost(new int[]{1, 2, 4, 5, 3, 6, 7},
                 new int[]{4, 5, 2, 6, 7, 3, 1}).asList());
         System.out.println(hIndex(new int[]{3, 0, 6, 1, 5}));
+        System.out.println(mostProfitablePath(new int[][]{{0,1},{1,2},{1,3},{3,4}}, 3, new int[]{-2,4,2,-4,6}));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
