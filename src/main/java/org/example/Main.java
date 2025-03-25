b4f7e381aa624263f58c809b37439144e3567d61
Константин <zed726804@yandex.ru>
2025-03-25 22:03:59 +0300

    feat: решение 3-ей задачи 197-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
@@ -1067,13 +1066,14 @@ public class Main {
                 new ListNode(2, new ListNode(3)))))).asList());
         SqlExecutor.execute("SolutionDay195Third.sql");
 
-        System.out.println(countDays(10, new int[][]{{5,7},{1,3},{9,10}}));
+        System.out.println(countDays(10, new int[][]{{5, 7}, {1, 3}, {9, 10}}));
         System.out.println(rotateRight(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4,
                 new ListNode(5))))), 2).asList());
         SqlExecutor.execute("SolutionDay196Third.sql");
-        System.out.println(checkValidCuts(5, new int[][]{{1,0,5,2},{0,2,2,4},{3,2,5,3},{0,4,4,5}}));
+        System.out.println(checkValidCuts(5, new int[][]{{1, 0, 5, 2}, {0, 2, 2, 4}, {3, 2, 5, 3}, {0, 4, 4, 5}}));
         System.out.println(SolutionDay197Second.maxDepth(new TreeNode(3, new TreeNode(9),
                 new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+        SqlExecutor.execute("SolutionDay197Third.sql");
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
