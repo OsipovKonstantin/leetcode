8b5004e4ef1b72f2720a9a79dc2481dafafabde4
Константин <zed726804@yandex.ru>
2025-04-17 10:32:58 +0300

    feat: решение 1-ой задачи 214-го дня

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1164,6 +1164,7 @@ public class Main {
         System.out.println(SolutionDay213Second.Companion.kthSmallest(new TreeNode(3,
                 new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)), 1));
         SqlExecutor.execute("SolutionDay213Third.sql");
+        System.out.println(SolutionDay214.Companion.countPairs(new int[]{3, 1, 2, 2, 2, 1, 3}, 2));
 
         Instant end = Instant.now();
         System.out.println(String.format("длительность всех задач в миллисекундах %d",
